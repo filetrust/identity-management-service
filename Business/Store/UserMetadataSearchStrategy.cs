ffdf15bb10bee17c75695b5df3e7187e68f4d3e9
Sebastian Birch <sbirch@glasswallsolutions.com>
2021-01-06 14:31:31 +0000

    Add search protection to allow other data to be placed in the share and not break the app

---
+++ Business/Store/UserMetadataSearchStrategy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Glasswall.IdentityManagementService.Common.Store;
+
+namespace Glasswall.IdentityManagementService.Business.Services
+{
+    public class UserMetadataSearchStrategy : IPathActions
+    {
+        public PathAction DecideAction(string path)
+        {
+            if (path?.Split("/")?.LastOrDefault().Split(".").FirstOrDefault(x => Guid.TryParse(x, out _)) == null)
+                return PathAction.Continue;
+
+            return PathAction.Collect;
+        }
+    }
+}

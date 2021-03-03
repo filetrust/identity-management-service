505651d74871f824763577a004b07add599c0c5a
gw-sbirch <60774443+gw-sbirch@users.noreply.github.com>
2021-03-03 13:59:17 +0000

    Deploying to gh-pages from  @ 74b8feec21845dcf21a78ad94fe98998ab5160e3 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/identity-management-service/precache-manifest.01bad3cace4ce85c7af765565f85c9cf.js"
+  "/identity-management-service/precache-manifest.ebb884f7f62aa229f91978e1319f85a6.js"
 );
 
 self.addEventListener('message', (event) => {
